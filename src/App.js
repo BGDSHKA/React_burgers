5d1ee47bbf468cbf44c783eb7967eafaf59c6a2d
Kirill Ateev <kirill.ateev59@gmail.com>
2020-11-15 12:38:58 +0300

    feature Products

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { GlobalStyle } from './globalStyle';
 import Hero from './Components/Hero';
+import { productData } from './Components/Products/data';
+import Products from './Components/Products'
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading='Choose your favorite' data={productData}/>
     </Router>
   );
 }
---
+++ src/Components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../img/product1.jpg'
+import product2 from '../../img/product2.png'
+import product3 from '../../img/product3.jpg'
+
+export const productData = [
+{
+    img: product1,
+    alt: 'Burger',
+    name: 'Cheesy cheese',
+    desc: 'Explosive cheese flavor, prime beef and legendary sauce',
+    price: '$4.99',
+    button: 'Add to Cart'
+},
+{
+    img: product2,
+    alt: 'Burger',
+    name: 'Baconator',
+    desc: 'Rare beef, grilled bacon, smoked cheese and special sauce',
+    price: '$5.99',
+    button: 'Add to Cart'
+},
+{
+    img: product3,
+    alt: 'Burger',
+    name: 'Supreme',
+    desc: 'An unforgettable taste that you want more and more',
+    price: '$7.99',
+    button: 'Add to Cart'
+}
+]
