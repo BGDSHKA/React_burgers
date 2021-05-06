f53356ca764d53249be7e9044878f352ffec225a
Kirill Ateev <Kirill.ateev59@gmail.com>
2021-05-06 18:53:56 +0500

    formatting

--- src/Components/Feature/index.js
+++ src/Components/Feature/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import {FeatureContainer, FeatureButton} from './FeatureElements'
+import React from "react";
+import { FeatureContainer, FeatureButton } from "./FeatureElements";
 
 const Feature = () => {
-    return (
-        <FeatureContainer>
-            <h1>Free fries and sauce when ordering</h1>
-            <p>Choose ketchup or thousand islands sauce</p>
-            <FeatureButton>Order Now</FeatureButton>
-        </FeatureContainer>
-    )
-}
+  return (
+    <FeatureContainer>
+      <h1>Free fries and sauce when ordering</h1>
+      <p>Choose ketchup or thousand islands sauce</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
+};
 
-export default Feature
+export default Feature;
